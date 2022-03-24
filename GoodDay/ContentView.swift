3766b0449302b05305d9fc7d5ac6ea9501a5ddd0
Yossa Bourne <cybercanonx@gmail.com>
2022-03-24 21:25:38 +0700

    chore: first commit

---
+++ GoodDay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoodDay
+//
+//  Created by Art on 24/3/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoodDay/GoodDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodDayApp.swift
+//  GoodDay
+//
+//  Created by Art on 24/3/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct GoodDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
